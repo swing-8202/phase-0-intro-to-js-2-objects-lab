01cda207f01bfa2b0ae3bb6ac9730af691fb2795
Sokung <swing.sophie.8202@gmail.com>
2021-03-25 17:57:57 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sophie',
+    streetAddress: '5100 California'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeNew = {...employee};
+    employeeNew[key] = value;
+    return employeeNew
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeClone = {...employee};
+    delete employeeClone[key];
+    return employeeClone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
